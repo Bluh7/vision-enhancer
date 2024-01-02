55d9f689fbfcc9f2c5649e7bb8d1b8398716a0fa
Bluh7 <gabrielperazzo123456789@gmail.com>
2024-01-02 17:29:36 -0300

    refactored users_running helper

--- src/helpers/users_running.py
+++ src/helpers/users_running.py
@@ -1,18 +1,17 @@
 class UsersRunning:
     def __init__(self) -> None:
-        self.__users_running = []
+        self.__users_running: set[str] = set()
 
     @property
-    def users_running_list(self) -> list:
-        return self.__users_running
+    def users_running_list(self) -> list[str]:
+        return list(self.__users_running)
 
     def add_new_user_running(self, author_id: str) -> None:
-        self.__users_running.append(author_id)
+        if author_id in self.__users_running:
+            raise ValueError(f"{author_id} is already in users_running")
+        self.__users_running.add(author_id)
 
     def remove_user_running(self, author_id: str) -> None:
-        if not self.__is_author_id_in_users_running(author_id):
+        if author_id not in self.__users_running:
             raise ValueError(f"{author_id} is not in users_running")
         self.__users_running.remove(author_id)
-
-    def __is_author_id_in_users_running(self, author_id: str) -> bool:
-        return author_id in self.__users_running
