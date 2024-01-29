ebe1fcd5589c82ae250f1d22a9ae9653c0237c89
Blu <gabrielperazzob@proton.me>
2024-01-28 23:32:38 -0300

    Cogs load bug fix

--- src/main.py
+++ src/main.py
@@ -13,12 +13,7 @@ CLIENT = discord.Bot(intents=INTENTS)
 @CLIENT.event
 async def on_connect():
     print(f"Connected to discord as {CLIENT.user}")
-
-
-@CLIENT.event
-async def on_ready():
-    CLIENT.load_extensions("cogs")
     await CLIENT.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="/upscale"))
 
-
+CLIENT.load_extensions("cogs")
 CLIENT.run(os.getenv("BOT_TOKEN"))
